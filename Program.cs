b91a8eb8551e0b8f298e795a083c65757106e7ef
Kauã <kau.lira@yahoo.com>
2022-04-08 19:50:52 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿int numero; 
+string parimpar; 
+
+Console.Write("digite seu numero: ");
+numero = Convert.ToInt32(Console.ReadLine());
+
+parimpar = (numero % 2) == 0? "par": "ímpar"; 
+
+Console.WriteLine($"{numero} é {parimpar}");
